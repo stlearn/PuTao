d071d1df625d0c017926e3d6055c955c2015757f
yunfei gao <2649793491@qq.com>
2020-07-29 20:44:33 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PuTao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                i++;
+                Console.Write("\u250c\u2510\u2514\u2518");
+            }
+
+        }
+    }
+}
